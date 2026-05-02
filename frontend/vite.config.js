c0acaa72365bb6ffe4793f61563ec3d098dc0e41
William Nguyen <williamgky@gmail.com>
2026-05-02 11:58:53 -0700

    vite config2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "https://github.com/wn80414/CMPE259-NLP/tree/main/frontend"
+  base: process.env.VITE_BASE_PATH || "/"
 })
